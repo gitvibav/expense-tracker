b42fd7d7d926fde3a031e486753ad66e6af6af96
gitvibav-koolio <128117178+gitvibav@users.noreply.github.com>
2026-03-06 12:23:35 +0530

    added render files

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_03_06_060429) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "expense_item_shares", force: :cascade do |t|
     t.integer "amount_cents"
     t.datetime "created_at", null: false
@@ -58,7 +61,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_06_060429) do
     t.string "name"
     t.string "password_digest", null: false
     t.datetime "updated_at", null: false
-    t.index "lower(email)", name: "index_users_on_lower_email", unique: true
+    t.index "lower((email)::text)", name: "index_users_on_lower_email", unique: true
   end
 
   add_foreign_key "expense_item_shares", "expense_items"
